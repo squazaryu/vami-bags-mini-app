c46ed34cc19dea54a065d7ab1b3d4fa9a3d33deb
tumowuh <tumowuh@MacBook-Pro-tumowuh.local>
2025-03-20 22:52:10 +0300

    Remove: Render deployment files

--- start.py
+++
@@ -1,9 +0,0 @@
-import os
-from server import app
-
-if __name__ == '__main__':
-    # Получаем порт из переменных окружения Render
-    port = int(os.environ.get('PORT', 8000))
-    
-    # Запускаем приложение
-    app.run(host='0.0.0.0', port=port) 
